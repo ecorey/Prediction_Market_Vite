58aee200a488f0ef63e67e702be6ee356594e1fd
ecorey <bengstrom32@gmail.com>
2024-03-16 21:35:13 -0400

    all

---
+++ scripts/start_game.js
@@ -0,0 +1,268 @@
+// imports
+import { getFullnodeUrl, SuiClient, SuiHTTPTransport  } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import walletDev from './dev-wallet.json' assert { type: 'json' };
+
+import { WebSocket } from 'ws';
+
+
+
+// generate a keypair
+const privateKeyArray = walletDev.privateKey.split(',').map(num => parseInt(num, 10));
+const privateKeyBytes = new Uint8Array(privateKeyArray);
+const keypairdev = Ed25519Keypair.fromSecretKey(privateKeyBytes);
+
+
+// PUBLISH MOVE CONTRACT THEN SET THE TARGET AND UPDATE MOVE CALLS TO USE THE NEW TARGET
+// SET: the consts
+// GET: current time
+// CREATE: predict epoch and report epoch with the correct times and use them to start the game
+// GET: the game balance
+
+// CLOSE: the game with the game id and a game result
+// WITHDRAW: balance from the game
+// DELETE: the game cap 
+
+
+// CONSTS
+const PACKAGE_ID = "";
+const itemType = `${PACKAGE_ID}::kiosk_practice::Prediction`;
+
+// pub key for wallet-three used to publish the package
+const devPubwallet = "";
+const tx_block = "";
+
+// const for the move calls
+const clock = "0x6";
+
+//  created in init
+const start_game_cap = "";
+const end_game_cap = "";
+const game_owner_cap = "";
+const transfer_policy = "";
+const transfer_policy_cap = "";
+const publisher = "";
+const upgrade_cap = "";
+
+
+// TIME STAMPS (election event on Nov. 5)
+// get from get time event log
+const predict_start_time = 1710631841064;
+// November 3, 2024, at 12:00 AM (GMT)
+const predict_end_time = 1730592000000;
+
+
+// November 10, 2024 at 12:00 AM (GMT)
+const report_start_time = 1731196800000;
+// December 1, 2024 at 12:00 AM (GMT)
+const report_end_time = 1733011200000;
+ 
+const ownerAddress = '0x07095af51002db0e9be284b8dab97263f77fec2a1be68cd42b7dd2358a6eccdd'; 
+
+const game_price = 1000000;
+
+
+const game_id ="";
+
+
+
+
+
+// client
+const client = new SuiClient({
+    transport: new SuiHTTPTransport({
+        url: getFullnodeUrl('testnet'),
+        WebSocketConstructor: WebSocket
+    }),
+});
+
+
+
+
+
+
+// const coins = await client.getCoins({ owner: ownerAddress });
+// console.log(`Coins owned by ${ownerAddress}:`, JSON.stringify(coins, null, 2));
+
+
+
+(async () => {
+    try {
+     
+        
+        // create Transaction Block
+        const txb = new TransactionBlock();
+
+
+
+
+
+
+
+
+        // GET THE CURRENT TIME
+
+        // async function logCurrentTime() {
+        //     await txb.moveCall({
+        //         target: `${PACKAGE_ID}::kiosk_practice::get_time`,
+        //         arguments: [txb.object(clock)],
+        //     });
+           
+        // }
+
+        // await logCurrentTime();
+        
+        
+
+
+
+
+
+        // CREATE PREDICT EPOCH AND REPORT EPOCH
+
+        // const predict_epoch = txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::set_predict_epoch`,
+        //     arguments: [ txb.pure.u64(predict_start_time), txb.pure.u64(predict_end_time) ],
+        // });
+
+
+        // const report_epoch = txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::set_report_epoch`,
+        //     arguments: [ txb.pure.u64(report_start_time), txb.pure.u64(report_end_time) ],
+        // });
+
+
+
+
+
+        // START THE GAME
+        
+        // txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::start_game`,
+        //     arguments: [ txb.object(start_game_cap), txb.pure.u64(game_price), txb.object(predict_epoch), txb.object(report_epoch), txb.object(clock)],
+        // });
+
+
+
+
+
+        // GET THE GAME BALANCE VIA EVENT LOG [TEST]
+
+        // async function logGameBalance() {
+            
+        //      await txb.moveCall({
+        //         target: `${PACKAGE_ID}::kiosk_practice::get_game_balance`,
+        //         arguments: [ txb.object(game_id) ],
+        //     });
+           
+        // }
+
+        // await logGameBalance();
+        
+        
+
+
+
+
+        // ADD A BALANCE TO THE GAME
+
+        // const coin_to_add = "0x1a3959723e52ecad24672e1812e718c2ca8cf6d9aaecedbd2fe80993998c12e4";
+
+        // txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::add_game_balance`,
+        //     arguments: [ txb.object(game_id), txb.object(coin_to_add) ],
+        // });
+
+
+
+
+
+        //WITHDRAW THE BALANCE FROM THE GAME [TEST]
+
+        // let amount = 518295497;
+
+        // txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::withdraw_balance_from_game`,
+        //     arguments: [ txb.object(game_id), txb.pure.u64(amount)],
+        // });
+
+        
+
+
+        // CLOSE THE GAME
+
+        // const game_result = 232;
+
+        // txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::close_game`,
+        //     arguments: [ txb.object(end_game_cap), txb.object(game_id), txb.pure.u64(game_result)],
+        // });
+
+
+
+
+
+        // DELETE THE GAME OWNER CAP
+
+        // txb.moveCall({
+        //     target: `${PACKAGE_ID}::kiosk_practice::delete_game_owner_cap`,
+        //     arguments: [ txb.object(game_owner_cap)],
+        // });
+
+
+
+
+
+        
+
+        
+    
+        
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        // finalize the transaction block
+        let txid = await client.signAndExecuteTransactionBlock({
+            signer: keypairdev,
+            transactionBlock: txb,
+        });
+        
+
+
+        // // log the transaction result
+        console.log(`Transaction result: ${JSON.stringify(txid, null, 2)}`);
+        console.log(`success: https://suiexplorer.com/txblock/${txid.digest}?network=testnet`);
+
+
+
+        console.log(`END OF SCRIPT`);
+
+    } catch (e) {
+        console.error(`error: ${e}`);
+    }
+})();
+
+
+
+
---
+++ scripts/subscibe_events.js
@@ -0,0 +1,75 @@
+// Imports
+import { getFullnodeUrl, SuiClient, SuiHTTPTransport } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { WebSocket } from 'ws';
+import walletDev from './dev-wallet.json' assert { type: 'json' };
+
+
+
+// Initialize keypair
+const privateKeyArray = walletDev.privateKey.split(',').map(num => parseInt(num, 10));
+const privateKeyBytes = new Uint8Array(privateKeyArray);
+const keypairdev = Ed25519Keypair.fromSecretKey(privateKeyBytes);
+
+
+
+// Constants
+const Package = "0xf5733f359175cb8f6b08fdc927a7dab4680d2255788a83f9afbe748d08602b99";
+
+
+
+
+// Client connection to Sui testnet
+const client = new SuiClient({
+    transport: new SuiHTTPTransport({
+        url: getFullnodeUrl('testnet'),
+        WebSocketConstructor: WebSocket
+    }),
+});
+
+
+
+
+(async () => {
+    try {
+
+
+        console.log(
+            await client.getObject({
+                id: Package,
+                options: { showPreviousTransaction: true },
+            }),
+        );
+
+
+
+
+        // let unsubscribe = await client.subscribeEvent({
+        //     filter: { Package },
+        //     onMessage: (event) => {
+        //         console.log('subscribeEvent', JSON.stringify(event, null, 2));
+        //     },
+        // });
+         
+       
+
+        // await unsubscribe();
+
+
+
+
+
+        
+
+
+        console.log(`END OF SCRIPT`);
+
+        
+    } catch (e) {
+        console.error(`error: ${e}`);
+    }
+
+})();
+
+
+
