a989a3b1a27922c1ad27aa417cf360d7233c499f
ecorey <bengstrom32@gmail.com>
2024-03-23 17:40:29 -0400

    kiosk

--- src/components/second_page/AboutKiosk.tsx
+++ src/components/second_page/AboutKiosk.tsx
@@ -31,8 +31,63 @@ const AboutKiosk = () => {
       </Typography>
       
       <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
-        Predictrix is a decentralized application that allows users to predict the outcome of the 2024 US Presidential Election using SUI. Predictrix is currently in a test phase and is for educational purposes only. No actual monetary value is exchanged. 
+        Predictrix is a decentralized application that leverages the power of SUI and uses the SUI kiosk to manage predictions made on this site. You can connect your wallet to manage your prediction using the KIOSK here. 
       </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To create a new Kiosk and Place the Prediction in the kiosk see below.      
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To Take your Prediction from the Kiosk see below.      
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To List a Prediction in the Kiosk see below.
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To Delist a Prediction from the Kiosk see below.      
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To Purchase a Prediction from the Kiosk see below.      
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        ***
+      </Typography>
+
+      <Typography className={cn("px-2 py-2 m-2")}  variant="body1" gutterBottom component="div" sx={{ color: 'white', }}>
+        To Withdraw funds from your Kiosk see below.      
+      </Typography>
+
+      
+
+
+
+
+
     </Box>
     </ThemeProvider>
   );
