791f1354985b0cf5eedaac1b602596e51735302c
ecorey <bengstrom32@gmail.com>
2024-03-25 00:05:02 -0400

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -15,8 +16,14 @@ const Footer = () => {
     <ThemeProvider theme={theme}>
     <Box sx={{  p: 3, bottom: 0, width: '100%',  }}>
       <Typography variant="body1" color="red" align="center">
-        © 2024 PREDICTRIX.
+        © 2024 PREDICTRIX.MOV
       </Typography>
+      <Typography variant="body1" color="red" align="center">
+          GitHub:
+          <Link href="https://github.com/ecorey/Prediction_Market_Vite/tree/main" color="inherit" underline="hover">
+             -PACKAGE SCRIPTS AND TESTS-
+          </Link>
+        </Typography>
     </Box>
     </ThemeProvider>
   );
